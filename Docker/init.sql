b6ab9b7a9253585832cde8c166e39191ce85451f
PROD\dliburdi <daniele.liburdi@alten.it>
2023-03-27 16:13:41 +0200

    fixes sql file from EXIST TO EXISTS

--- Docker/init.sql
+++ Docker/init.sql
@@ -1,23 +1,19 @@
-CREATE USER docker;
-CREATE DATABASE pawtropolis;
-GRANT ALL PRIVILEGES ON DATABASE pawtropolis TO docker;
-
-CREATE TABLE IF NOT EXIST game_map(
+CREATE TABLE IF NOT EXISTS game_map(
 id INT PRIMARY KEY NOT NULL,
 name VARCHAR(24) UNIQUE NOT NULL);
 
-CREATE TABLE IF NOT EXIST item(
+CREATE TABLE IF NOT EXISTS item(
 Id INT PRIMARY KEY NOT NULL,
 name VARCHAR(24) UNIQUE NOT NULL,
-description VARCHAR(64) NOT NULL);
+description VARCHAR(64) NOT NULL,
 volume INT NOT NULL
 );
 
-CREATE TABLE IF NOT EXIST type_room(
+CREATE TABLE IF NOT EXISTS type_room(
 id INT PRIMARY KEY NOT NULL,
 name VARCHAR(24) UNIQUE NOT NULL);
 
-CREATE TABLE IF NOT EXIST room(
+CREATE TABLE IF NOT EXISTS room(
 id INT PRIMARY KEY NOT NULL,
 name VARCHAR(24) UNIQUE NOT NULL,
 position_x INT NOT NULL,
@@ -26,25 +22,25 @@ type_id INT NOT NULL references type_room(id),
 map_id INT NOT NULL references game_map(id)
 );
 
-CREATE TABLE IF NOT EXIST direction(
+CREATE TABLE IF NOT EXISTS direction(
 id INT PRIMARY KEY NOT NULL,
 name VARCHAR(24) UNIQUE NOT NULL);
 
-CREATE TABLE IF NOT EXIST room_connection(
+CREATE TABLE IF NOT EXISTS room_connection(
 id INT PRIMARY KEY NOT NULL,
 from_room_id INT NOT NULL references room(id),
 direction_id INT NOT NULL references direction(id),
 to_room_id INT NOT NULL references room(id)
 );
 
-CREATE TABLE IF NOT EXIST item_room(
+CREATE TABLE IF NOT EXISTS item_room(
 Id INT PRIMARY KEY NOT NULL,
 Quantity INT NOT NULL,
 Item_id INT NOT NULL references item(id),
 Room_id INT NOT NULL references room(id)
 );
 
-CREATE TABLE IF NOT EXIST player(
+CREATE TABLE IF NOT EXISTS player(
 Id INT PRIMARY KEY NOT NULL,
 name VARCHAR(24) NOT NULL,
 position_x INT NOT NULL,
@@ -52,14 +48,14 @@ position_y INT NOT NULL,
 lifepoints INT NOT NULL
 )
 
-CREATE TABLE IF NOT EXIST bag(
+CREATE TABLE IF NOT EXISTS bag(
 Id INT PRIMARY KEY NOT NULL,
 volume INT NOT NULL,
 occupied_volume INT NOT NULL,
 player_id INT references player(id)
 );
 
-CREATE TABLE IF NOT EXIST item_bag(
+CREATE TABLE IF NOT EXISTS item_bag(
 Id INT PRIMARY KEY NOT NULL,
 quantity INT NOT NULL,
 item_id INT NOT NULL references item(id),
