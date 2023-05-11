7cba4574bd5e172e6e47ac940b3b4ac3f1ce4183
Livarbesaru <liburdidaniele1998@gmail.com>
2023-05-11 17:03:58 +0200

    fix ( BagDTO not proprely related in PlayerDTO )

--- src/main/java/pawtropolis/model/dto/entity/PlayerDTO.java
+++ src/main/java/pawtropolis/model/dto/entity/PlayerDTO.java
@@ -1,6 +1,7 @@
 package pawtropolis.model.dto.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +13,8 @@ import pawtropolis.model.dto.items.InventoryDTO;
 @Entity
 @Table(name = "player")
 public class PlayerDTO extends EntityDTO{
+
+	@OneToOne
 	private BagDTO bagDTO;
 	public PlayerDTO() {
 		this.bagDTO = new BagDTO(new InventoryDTO());
